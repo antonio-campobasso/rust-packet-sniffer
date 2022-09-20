676a475223a35e2bb746287307531f29debbea67
simog19 <simone.galota19@gmail.com>
2022-09-20 04:34:17 +0200

    Started implementing API in lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,91 @@
+use pcap::{Device, Capture, Packet, PacketHeader};
+use pktparse::{ethernet, ipv4, tcp, udp, ip};
+use std::net::Ipv4Addr;
+
+pub struct TupleReport {
+    pub ts_first: libc::timeval,
+    pub ts_last: libc::timeval,
+    pub src: Ipv4Addr,
+    pub dst: Ipv4Addr,
+    pub src_port: u16,
+    pub dst_port: u16,
+    pub protocol: String,
+    pub bytes: i64,
+    //pub descr: String,
+}
+
+//implementare tratto display per la struct tupleReport
+impl TupleReport {
+    pub fn new() -> Self {
+        TupleReport {
+            ts_first: libc::timeval { tv_sec: 0, tv_usec: 0 },
+            ts_last: libc::timeval { tv_sec: 0, tv_usec: 0 },
+            src: Ipv4Addr::new(0, 0, 0, 0),
+            dst: Ipv4Addr::new(0, 0, 0, 0),
+            src_port: 0,
+            dst_port: 0,
+            protocol: "".to_string(),
+            bytes: 0,
+            //descr: "".to_string(),
+        }
+    }
+    // Parsing headers and insert filter (decidere ancora dove implementare filter)
+}
+
+// Start capture --> default snaplen is 65535 (the maximum length of a packet captured into the buffer).
+pub fn start_capture(interface_name: &str) -> () { //esiste la filter e la stats per la struct Capture
+    let mut cap = Capture::from_device(interface_name).unwrap()// assume the device exists and we are authorized to open it
+        .promisc(true)
+        .open().unwrap();// activate the handle // assume activation worked
+    //check error in opening and starting a capture
+    println!("Sniffing process is active");
+
+    let mut tr = TupleReport::new();
+    while let Ok(packet) = cap.next_packet() { //fare controllo sul next packet
+        //let packet = cap.next_packet().unwrap();
+        //println!("{:?}", packet);
+        if let Ok((payload, frame)) = ethernet::parse_ethernet_frame(packet.data) {
+            println!("{:x?}", frame);
+            match frame.ethertype {
+                ethernet::EtherType::IPv4 => {
+                    if let Ok((payload, datagram)) = ipv4::parse_ipv4_header(payload) {
+                        println!("{:?}", datagram);
+                        //tr.src = datagram.source_addr;
+                        match datagram.protocol {
+                            ip::IPProtocol::TCP => {
+                                if let Ok((_payload, segment)) = tcp::parse_tcp_header(payload) {
+                                    println!("{:?}", segment);
+                                } else {
+                                    println!("Error parsing TCP segment.");
+                                }
+                            }
+                            ip::IPProtocol::UDP => {
+                                if let Ok((_payload, udp_datagram)) = udp::parse_udp_header(payload) {
+                                    println!("{:?}", udp_datagram);
+                                } else {
+                                    println!("Error parsing UDP datagram.");
+                                }
+                            }
+                            _ => { println!("L4 protocol not supported") }
+                        }
+                    } else {
+                        println!("Error parsing IP datagram.");
+                    }
+                }
+                _ => { println!("L3 protocol not supported") }
+            }
+        } else {
+            println!("Error parsing Ethernet frame.");
+        }
+    }
+}
+
+// list devices
+pub fn list_all_devices() -> Vec<Device> {
+    let devices = Device::list().unwrap();
+
+    for d in &devices {
+        println!("{:?}:{:?}", d.name, d.flags.connection_status);
+    }
+    devices
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use packet_sniffer::{list_all_devices, start_capture, TupleReport};
+
+//come mettere in pausa la capture? metto in pausa il thread che la sta eseguendo?
+//come riprendere la capture?
+//come considerare una connessione TCP/UDP, basta network/port pairs? Distinguere tra uplink e downlink?
+//come calcolare il numero totale di bytes trasmessi (suppongo da calcolare per ogni direzione)?
+//come faccio a sapere qual è la prima e l'ultimo pacchetto? uso SYN e FIN?
+//come gestire gli errori ?
+
 fn main() {
-    println!("Hello, world!");
+    //checking argument -> check error!
+    //check if interface has been specified as argument, if not
+    //"name": connection status
+    list_all_devices();
+
+    //1. insert a network interface connected -> Check error!
+    //2. start a capture -> indicate sniffing is ongoing
+    start_capture("en3"); // per adesso solo con ethernet
+    //2b. indicare che lo sniffing process è attivo dopo controlli errori
+    //3. mettere in pausa una capture
+    //4. riprendere una capture
+    //5. generare report testuale dopo X secondi in un file passato come argomento
+
+
+
 }
