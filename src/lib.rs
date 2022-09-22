1cd30791541f735aab709291a3ae1d2678f57a69
simog19 <simone.galota19@gmail.com>
2022-09-22 04:15:02 +0200

    code cleaning

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
-use pcap::{Device, Capture, ConnectionStatus};
+use pcap::{Device, Capture, ConnectionStatus, Packet, PacketHeader};
 use pktparse::{ethernet, ipv4, tcp, udp, icmp, arp};
 use std::net::Ipv4Addr;
 use std::string::ToString;
 use pktparse::ip::IPProtocol;
+use pktparse::ipv4::IPv4Header;
 
+//TODO racchiudere le due struct ConnInfo e ConnData in un'altra struct???
 #[derive(Eq, PartialEq, Hash, Debug)]
 pub struct ConnInfo {
     pub src: Ipv4Addr,
@@ -92,7 +94,7 @@ pub fn list_all_devices() -> Vec<Device> {
 }
 
 //service function to print the hashmap
-pub fn print_hashmap(hm: &HashMap<ConnInfo, ConnData>) -> () {
+fn print_hashmap(hm: &HashMap<ConnInfo, ConnData>) -> () {
     let mut i = 1;
 
     for (key, value) in hm {
@@ -101,104 +103,139 @@ pub fn print_hashmap(hm: &HashMap<ConnInfo, ConnData>) -> () {
     }
 }
 
+fn reporting(report: &mut HashMap<ConnInfo,ConnData>, datagram: IPv4Header, src: u16, dst: u16, packet_header: &PacketHeader, length: usize) -> () {
+    let ci = ConnInfo::new(datagram.source_addr, datagram.dest_addr, src, dst, datagram.protocol.tostring(), "".to_string());
+    let cd = ConnData::new(packet_header.ts, packet_header.ts, length + 38);
+    report.entry(ci)
+        .and_modify(|cd| {
+            cd.total_bytes += length + 38;
+            cd.ts_last = packet_header.ts
+        })
+        .or_insert(cd);
+}
+
+fn app_recognition_udp(src: u16, dst: u16) -> () {
+    if dst == 53 || src == 53 {
+        // println!("DNS message.");
+    } else if dst == 161 || src == 161 {
+        //println!("SNMP message.");
+    }
+}
+
+fn app_recognition_tcp(src: u16, dst: u16) -> () {
+    if dst == 80 || src == 80 {
+        //println!("HTTP message.");
+    } else if dst == 443 || src == 443 {
+        //println!("HTTPS message.");
+    } else if dst == 22 || src == 22 {
+        //println!("SSH message.");
+    }
+}
+
+fn parsing(report: &mut HashMap<ConnInfo,ConnData>,packet: Packet) -> () {
+    if let Ok((payload_e, frame)) = ethernet::parse_ethernet_frame(packet.data) {
+        //println!("{}", payload_e.len()); verifica di bytes effettivi trasmessi --> controllo payload del frame e aggiungo 38 (heaeder eth)
+        match frame.ethertype {
+            ethernet::EtherType::IPv4 => {
+                if let Ok((payload_i, datagram)) = ipv4::parse_ipv4_header(payload_e) {
+                    match datagram.protocol {
+                        IPProtocol::TCP => {
+                            if let Ok((_payload_t, segment)) = tcp::parse_tcp_header(payload_i) {
+                                reporting(report,datagram, segment.source_port, segment.dest_port, packet.header, payload_e.len());
+                                app_recognition_tcp(segment.source_port, segment.dest_port);
+                                //println!("{:?}", segment);
+                            } else {
+                                println!("Error parsing TCP segment.");
+                            }
+                        }
+                        IPProtocol::UDP => {
+                            if let Ok((_payload_u, udp_datagram)) = udp::parse_udp_header(payload_i) {
+                                reporting(report, datagram, udp_datagram.source_port, udp_datagram.dest_port, packet.header, payload_e.len());
+                                app_recognition_udp(udp_datagram.source_port, udp_datagram.dest_port);
+                                //println!("{:?}", udp_datagram);
+                            } else {
+                                println!("Error parsing UDP datagram.");
+                            }
+                        }
+                        IPProtocol::ICMP => { //TODO
+                            if let Ok((_payload, _packet)) = icmp::parse_icmp_header(payload_i) {
+                                println!("{:?}", _packet);
+                            } else {
+                                println!("Error parsing ICMP packet.");
+                            }
+                        }
+                        _ => { println!("L4 protocol not supported") }
+                    }
+                } else {
+                    println!("Error parsing IP datagram.");
+                }
+            }
+            //TODO ethernet::EtherType::IPv6 => {
+            // }
+            ethernet::EtherType::ARP => { //TODO da capire se inserire o meno nel report
+                if let Ok((_payload, _packet)) = arp::parse_arp_pkt(payload_e) {
+                    println!("{:x?}", _packet);
+                } else {
+                    println!("Error parsing ARP packet.");
+                }
+            }
+            _ => {
+                println!("L3 protocol not supported");
+            }
+        }
+    } else {
+        println!("Error parsing Ethernet frame.");
+    }
+}
+
+
 pub fn start_capture(interface_name: &str, bpf_program: &str) -> () {
     let mut cap = Capture::from_device(interface_name).unwrap()// TODO assume the device exists and we are authorized to open it
         .promisc(true)
-       // .snaplen(65535)
+        //.snaplen(65535)
         //.buffer_size(65)//serve per vedere subito output quando inviamo pochi dati, altrimenti non vedevo efficacia filtri
-        .open().unwrap()
-        ;//TODO check error in opening and starting a capture
+        .open().unwrap();//TODO check error in opening and starting a capture
 
     println!("Sniffing process in promiscuous mode is active on interface: {}", interface_name);
+    cap.filter(bpf_program, true).unwrap();
 
+    //TODO lasciare la dichiarazione dell'HashMap report dentro start_capture?
     let mut report: HashMap<ConnInfo, ConnData> = HashMap::new();
 
-    cap.filter(bpf_program, true).unwrap();
     while let Ok(packet) = cap.next_packet() { //TODO fare controllo sul next packet
+        parsing(&mut report, packet);
 
-        if let Ok((payload_e, frame)) = ethernet::parse_ethernet_frame(packet.data) {
-            //println!("{}", payload_e.len());
-            match frame.ethertype {
-                ethernet::EtherType::IPv4 => {
-                    if let Ok((payload_i, datagram)) = ipv4::parse_ipv4_header(payload_e) {
-                        match datagram.protocol {
-                            IPProtocol::TCP => {
-                                if let Ok((_payload_t, segment)) = tcp::parse_tcp_header(payload_i) {
-                                    //TODO da wrappare
-                                    let ci = ConnInfo::new(datagram.source_addr, datagram.dest_addr, segment.source_port, segment.dest_port, datagram.protocol.tostring(), "".to_string());
-                                    let cd = ConnData::new(packet.header.ts, packet.header.ts, payload_e.len() + 38);
-                                    report.entry(ci)
-                                        .and_modify(|cd| {
-                                            cd.total_bytes += payload_e.len() + 38;
-                                            cd.ts_last = packet.header.ts
-                                        })
-                                        .or_insert(cd);
-                                    println!("{:?}", segment);
-                                    if segment.dest_port == 80 || segment.source_port == 80 { //capire come considerare le porte
-                                        //println!("HTTP message.");
-                                    } else if segment.dest_port == 443 || segment.source_port == 443 {
-                                        //println!("HTTPS message.");
-                                    } else if segment.dest_port == 22 || segment.source_port == 22 {
-                                        //println!("SSH message.");
-                                    }
-                                } else {
-                                    println!("Error parsing TCP segment.");
-                                }
-                            }
-
-                            IPProtocol::UDP => {
-                                if let Ok((_payload_u, udp_datagram)) = udp::parse_udp_header(payload_i) {
-                                    //println!("{:?}", udp_datagram);
-                                    //TODO da wrappare in una funzione
-                                    let ci = ConnInfo::new(datagram.source_addr, datagram.dest_addr, udp_datagram.source_port, udp_datagram.dest_port, datagram.protocol.tostring(), "".to_string());
-                                    let cd = ConnData::new(packet.header.ts, packet.header.ts, payload_e.len() + 38);
-                                    report.entry(ci)
-                                        .and_modify(|cd| {
-                                            cd.total_bytes += payload_e.len() + 38;
-                                            cd.ts_last = packet.header.ts
-                                        })
-                                        .or_insert(cd);
-                                    println!("{:?}", udp_datagram);
-                                    if udp_datagram.dest_port == 53 || udp_datagram.source_port == 53 {
-                                        // println!("DNS message.");
-                                    } else if udp_datagram.dest_port == 161 || udp_datagram.source_port == 161 {
-                                        //println!("SNMP message.");
-                                    }
-                                } else {
-                                    println!("Error parsing UDP datagram.");
-                                }
-                            }
-
-                            IPProtocol::ICMP => { //TODO
-                                if let Ok((_payload, _packet)) = icmp::parse_icmp_header(payload_i) {
-                                    println!("{:?}", _packet);
-                                } else {
-                                    println!("Error parsing ICMP packet.");
-                                }
-                            }
-
-                            _ => { println!("L4 protocol not supported") }
-                        }
-                    } else {
-                        println!("Error parsing IP datagram.");
-                    }
-                }
-
-                //TODO ethernet::EtherType::IPv6 => {}
-                ethernet::EtherType::ARP => { //TODO manage for report
-                    if let Ok((_payload, _packet)) = arp::parse_arp_pkt(payload_e) {
-                        println!("{:x?}", _packet);
-                    } else {
-                        println!("Error parsing ARP packet.");
-                    }
-                }
-                _ => {
-                    println!("L3 protocol not supported");
-                }
-            }
-        } else {
-            println!("Error parsing Ethernet frame.");
-        }
+        //fare il ciclo while all'interno di un'altra funzione????
     }
+    print_hashmap(&report);
 }
+
+
+
+
+/*let ci = ConnInfo::new(datagram.source_addr, datagram.dest_addr, udp_datagram.source_port, udp_datagram.dest_port, datagram.protocol.tostring(), "".to_string());
+                                let cd = ConnData::new(packet.header.ts, packet.header.ts, payload_e.len() + 38);
+                                report.entry(ci)
+                                    .and_modify(|cd| {
+                                        cd.total_bytes += payload_e.len() + 38;
+                                        cd.ts_last = packet.header.ts
+                                    })
+                                    .or_insert(cd);*/
+//al suo interno chiamare funzione per riconoscimento livello Applicazione
+
+/*if segment.dest_port == 80 || segment.source_port == 80 { //capire come considerare le porte
+                            //println!("HTTP message.");
+                        } else if segment.dest_port == 443 || segment.source_port == 443 {
+                            //println!("HTTPS message.");
+                        } else if segment.dest_port == 22 || segment.source_port == 22 {
+                            //println!("SSH message.");
+                        }
+
+
+                        if udp_datagram.dest_port == 53 || udp_datagram.source_port == 53 {
+                            // println!("DNS message.");
+                        } else if udp_datagram.dest_port == 161 || udp_datagram.source_port == 161 {
+                            //println!("SNMP message.");
+                        }*/
+
 
