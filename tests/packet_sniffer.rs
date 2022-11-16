ae3afc31e54707c38d4e58fcf8b6922785107724
Centola Antonio <s288192@studenti.polito.it>
2022-11-16 10:41:07 +0100

    Tests implementation

--- tests/packet_sniffer.rs
+++ tests/packet_sniffer.rs
@@ -1,5 +1,15 @@
 use libc::timeval;
 use packet_sniffer::*;
+use std::io::ErrorKind;
+
+macro_rules! assert_err {
+    ($expression:expr, $($pattern:tt)+) => {
+        match $expression {
+            $($pattern)+ => (),
+            ref e => panic!("expected `{}` but got `{:?}`", stringify!($($pattern)+), e),
+        }
+    }
+}
 
 #[test]
 fn conn_info_created_with_valid_values() {
@@ -11,35 +21,93 @@ fn conn_info_created_with_valid_values() {
     assert_eq!(conn_info.protocol,"UDP".to_string());
     assert_eq!(conn_info.app_descr,"Some messages".to_string());
 }
+#[test]
+#[should_panic]
+fn conn_info_created_with_invalid_src() {
+    let conn_info = ConnInfo::new("me".to_string(),"you".to_string(),1,2, "UDP".to_string(),"Some messages".to_string());
+    assert_eq!(conn_info.src,"wrong".to_string());
+    assert_eq!(conn_info.dst,"you".to_string());
+    assert_eq!(conn_info.src_port,1);
+    assert_eq!(conn_info.dst_port,2);
+    assert_eq!(conn_info.protocol,"UDP".to_string());
+    assert_eq!(conn_info.app_descr,"Some messages".to_string());
+}
 
 #[test]
-fn conn_data_created_with_valid_values() {
+#[should_panic]
+fn conn_info_created_with_invalid_src_port() {
+    let conn_info = ConnInfo::new("me".to_string(),"you".to_string(),1,2, "UDP".to_string(),"Some messages".to_string());
+    assert_eq!(conn_info.src,"me".to_string());
+    assert_eq!(conn_info.dst,"you".to_string());
+    assert_eq!(conn_info.src_port,12);
+    assert_eq!(conn_info.dst_port,2);
+    assert_eq!(conn_info.protocol,"UDP".to_string());
+    assert_eq!(conn_info.app_descr,"Some messages".to_string());
+}
+#[test]
+#[should_panic]
+fn conn_info_created_with_invalid_dst() {
+    let conn_info = ConnInfo::new("me".to_string(),"you".to_string(),1,2, "UDP".to_string(),"Some messages".to_string());
+    assert_eq!(conn_info.src,"me".to_string());
+    assert_eq!(conn_info.dst,"wrong".to_string());
+    assert_eq!(conn_info.src_port,1);
+    assert_eq!(conn_info.dst_port,2);
+    assert_eq!(conn_info.protocol,"UDP".to_string());
+    assert_eq!(conn_info.app_descr,"Some messages".to_string());
+}
+#[test]
+#[should_panic]
+fn conn_info_created_with_invalid_dst_port() {
+    let conn_info = ConnInfo::new("me".to_string(),"you".to_string(),1,2, "UDP".to_string(),"Some messages".to_string());
+    assert_eq!(conn_info.src,"me".to_string());
+    assert_eq!(conn_info.dst,"you".to_string());
+    assert_eq!(conn_info.src_port,1);
+    assert_eq!(conn_info.dst_port,21);
+    assert_eq!(conn_info.protocol,"UDP".to_string());
+    assert_eq!(conn_info.app_descr,"Some messages".to_string());
+}
+
+
+#[test]
+#[should_panic]
+fn conn_data_created_with_invalid_values() {
     let conn_data = ConnData::new(timeval { tv_sec: (1), tv_usec: (0) },timeval { tv_sec: (2), tv_usec: (1) },10);
     assert_eq!(conn_data.ts_first.tv_sec as u64, 1);
     assert_eq!(conn_data.ts_first.tv_usec as u64, 0);
     assert_eq!(conn_data.ts_last.tv_sec as u64, 2);
     assert_eq!(conn_data.ts_last.tv_usec as u64, 1);
-    assert_eq!(conn_data.total_bytes, 10);
+    assert_eq!(conn_data.total_bytes, 12);
 }
 
-
-/* 
+/*
 #[test]
 fn packet_data_created_with_valid_values() {
     let packet_data = PacketData::new("8.8.8.8".to_string(), "1.1.1.1".to_string(), "me".to_string(), "you".to_string(), "UDP".to_string(), &(timeval { tv_sec: (2), tv_usec: (1)}, {12}, {12}).into() , 12, "Some messages".to_string());
     let conn_info = ConnInfo::new("me".to_string(),"you".to_string(),1,2, "UDP".to_string(),"Some messages".to_string());
     let conn_data = ConnData::new(timeval { tv_sec: (1), tv_usec: (0) },timeval { tv_sec: (2), tv_usec: (1) },10);
 
+} */
+
+/*#[test]
+#[should_panic]
+fn capture_device_created_with_invalid_interface_name() {
+    let cap_d = CaptureDevice::new("eth0".to_string(), None).unwrap();
+}
+ */
+/* 
+#[test]
+fn capture_device_created_with_valid_values() {
+    let interface_name = "eth0".into();
+    let cap_d = CaptureDevice::new(interface_name, None);
+    assert_err!(cap_d, Err(NetworkInterfaceError::CaptureDeviceOpeningError(_)));
 }*/
 
-/*
+/* 
 #[test]
 #[should_panic]
-fn capture_device_created_with_valid_values() {
-    let cap_d = CaptureDevice::new("eth0".to_string(), None).unwrap_err().kind();
-let expected_error_kind = NetworkInterfaceError:: CaptureDeviceOpeningError;
-assert!(cap_d.
-}, expected_error_kind);
+fn capture_device_created_with_invalid_interface_name() {
+    let cap_d = CaptureDevice::new("eth222".to_string(), None).unwrap();
 }
 */
+
 
