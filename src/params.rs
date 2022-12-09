ca9274e65df6384837245dcb6552f710a2f1275f
Antonio Campobasso <campobassoantonio7@gmail.com>
2022-12-10 00:51:17 +0100

    main program updates

--- src/params.rs
+++ src/params.rs
@@ -1,9 +1,7 @@
-use packet_sniffer::*;
 use clap::Parser;
+use packet_sniffer::*;
 
-use std::{
-    io::stdin,
-};
+use std::io::stdin;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -12,20 +10,20 @@ struct Args {
     #[clap(short, long, action, value_name = "guided mode")]
     guided: bool,
 
-    /// Network interface to perform the sniffing operation. Defaults to the first interface found.
+    /// Network interface to perform the sniffing operation.
     #[clap(value_parser, value_name = "network interface")]
-    network_interface: Option<String>,
+    network_interface: String,
 
-    /// Time interval between report writings. Defaults to 10 seconds.
+    /// Time interval between report writings.
     #[clap(value_parser, value_name = "time interval")]
-    time_interval: Option<usize>,
+    time_interval: usize,
 
-    /// Path of the file in which the report will be written. Defaults to "./report.txt".
+    /// Path of the file(without extension) in which the report will be written.
     #[clap(value_parser, value_name = "output file")]
-    file_name: Option<String>,
+    file_name: String,
 
     /// Filter to apply to sniffed packets
-    #[clap(value_parser, value_name = "filter")]
+    #[clap(short, long("filter"), value_parser, value_name = "filter")]
     filter_string: Option<String>,
 }
 
@@ -36,23 +34,10 @@ pub struct Params {
     pub filter: String,
 }
 
-#[derive(Debug)]
-pub enum Error {
-    NoDevicesError,
-    ParseError,
-    WrongFilterFormat,
-    WrongNameFormat,
-    OpeningError,
-}
-
-const DEFAULT_TIME_INTERVAL: usize = 10;
-const DEFAULT_FILE_NAME: &str = "report.txt";
-const DEFAULT_FILTER_STRING: &str = "not ip6 and not igmp";
-
-pub fn get_params() -> Result<Params, Error> {
-    let network_interface;
+pub fn get_params() -> Params {
+    let mut network_interface;
     let time_interval;
-    let file_name;
+    let mut file_name;
     let filter;
 
     let cli = Args::parse();
@@ -62,108 +47,94 @@ pub fn get_params() -> Result<Params, Error> {
         let devices = list_all_devices();
 
         // Network interface selection
-        println!("Select network interface from the following: ");
-        devices
-            .unwrap()
-            .iter()
-            .for_each(|device| println!("- {}", device.name));
+        loop {
+            buffer.clear();
 
-        stdin().read_line(&mut buffer).unwrap();
-        network_interface = buffer.trim().to_string();
+            println!("Select network interface from the following: ");
+            let interfaces: Vec<String> = devices
+                .as_ref()
+                .unwrap()
+                .iter()
+                .map(|device| device.name.clone())
+                .collect();
 
-        buffer.clear();
+            interfaces
+                .iter()
+                .for_each(|interface| println!("- {}", interface));
 
-        // Time interval
-        println!("Insert time interval between report production: ");
-        stdin().read_line(&mut buffer).unwrap();
+            stdin().read_line(&mut buffer).unwrap();
+            network_interface = buffer.trim().to_string();
 
-        if let Ok(val) = buffer.trim().parse() {
-            time_interval = val;
-        } else {
-            return Err(Error::ParseError);
+            if interfaces.contains(&network_interface) {
+                break;
+            }
+            println!("Interface not present in the list")
         }
 
-        buffer.clear();
+        // Time interval
+        loop {
+            buffer.clear();
+
+            println!("Insert time interval between report production: ");
+            stdin().read_line(&mut buffer).unwrap();
+
+            if let Ok(val) = buffer.trim().parse() {
+                time_interval = val;
+                break;
+            }
+
+            println!("Insert a valid number")
+        }
 
         // File name
-        println!("Insert file name for the report: ");
-        stdin().read_line(&mut buffer).unwrap();
-        file_name = buffer.trim().to_string();
+        loop {
+            buffer.clear();
+
+            println!("Insert file name for the report: ");
+            stdin().read_line(&mut buffer).unwrap();
+            file_name = buffer.trim().to_string();
+
+            if !file_name.is_empty() {
+                break;
+            }
+        }
 
         buffer.clear();
 
         // Filter
-        println!("Insert packet filter string: ");
+        println!("Insert packet filter string (Using BPF syntax): ");
         stdin().read_line(&mut buffer).unwrap();
 
         filter = buffer.trim().to_string();
 
-        return Ok(Params {
-            network_interface,
-            time_interval,
-            file_name,
-            filter,
-        });
-    }
-
-    match cli.network_interface {
-        Some(interface) => network_interface = interface,
-        None => {
-            network_interface = match list_all_devices() {
-                Ok(devices) => match devices.first() {
-                    Some(first_device) => {
-                        let interface = first_device.name.clone();
-                        println!("Interface: {}", interface);
-                        interface
-                    }
-                    None => return Err(Error::NoDevicesError),
-                },
-                Err(e) => {
-                    match e {
-                        NetworkInterfaceError::CaptureDeviceOpeningError(_) => return Err(Error::OpeningError),
-                        NetworkInterfaceError::WrongNameFormat(_) => return Err(Error::WrongNameFormat),
-                        NetworkInterfaceError::FilterError(_) => return Err(Error::WrongFilterFormat),
-                        NetworkInterfaceError::NoDevicesError(_) => return Err(Error::NoDevicesError),
-                    }
-                },
-            }
+        println!(
+            "Network interface: {}\nTime Interval: {}\nFile name: {}\nFilter: {}",
+            network_interface, time_interval, file_name, filter
+        );
+    } else {
+        network_interface = cli.network_interface.clone();
+        time_interval = cli.time_interval;
+        file_name = cli.file_name;
+        match cli.filter_string {
+            Some(f) => filter = f,
+            None => filter = "".to_string(),
         }
     }
 
-    match cli.time_interval {
-        Some(interval) => time_interval = interval,
-        None => {
-            time_interval = {
-                println!("Time interval: {}", DEFAULT_TIME_INTERVAL);
-                DEFAULT_TIME_INTERVAL
-            }
-        }
-    }
+    let print_filter = match filter.as_str() {
+        "" => "(None)".to_string(),
+        f => f.to_string(),
+    };
 
-    match cli.file_name {
-        Some(name) => file_name = name,
-        None => {
-            file_name = {
-                println!("File name: {}", DEFAULT_FILE_NAME);
-                DEFAULT_FILE_NAME.to_string()
-            }
-        }
-    }
+    println!(
+        "<---Parameters--->\nNetwork interface: {}\nTime Interval: {}\nFile name: {}.csv\nFilter: {}\n<---------------->",
+        network_interface, time_interval, file_name, print_filter
+    );
 
-    match cli.filter_string {
-        Some(f) => filter = f,
-        None => {
-            filter = {
-                println!("Filter: {}", DEFAULT_FILTER_STRING);
-                DEFAULT_FILTER_STRING.to_string()
-            }
-        }
-    }
-
-    return Ok(Params {
+    return Params {
         network_interface,
         time_interval,
         file_name,
         filter,
-    });
+    };
 }
